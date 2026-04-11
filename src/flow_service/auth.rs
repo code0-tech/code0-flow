55b7a07585c63e5fac5504169c3734183778013e
Raphael <raphael.goetz.7@gmail.com>
2026-04-12 01:22:19 +0200

    feat: added auth metadata function

---
+++ src/flow_service/auth.rs
@@ -0,0 +1,29 @@
+use std::str::FromStr;
+use tonic::metadata::{MetadataMap, MetadataValue};
+
+/// get_authorization_metadata
+///
+/// Creates a `MetadataMap` that contains the defined token as a value of the `authorization` key
+/// Used for setting the runtime_token to authorize Sagittarius request
+///
+/// # Examples
+///
+/// ```
+/// use aquila_grpc::get_authorization_metadata;
+/// let token = String::from("token");
+/// let metadata = get_authorization_metadata(&token);
+/// assert!(metadata.get("authorization").is_some());
+/// assert_eq!(metadata.get("authorization").unwrap(), "token");
+/// ```
+pub fn get_authorization_metadata(token: &str) -> MetadataMap {
+    let metadata_value = MetadataValue::from_str(token).unwrap_or_else(|error| {
+        panic!(
+            "An error occurred trying to convert runtime_token into metadata: {}",
+            error
+        );
+    });
+
+    let mut map = MetadataMap::new();
+    map.insert("authorization", metadata_value);
+    map
+}
